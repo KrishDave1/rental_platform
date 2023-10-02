bc8c939fc1588b04f373376074a0d8fb6b6ed5d2
valmikGit <quantumcomputingiiitb@gmail.com>
2023-10-02 23:50:43 +0530

    end ka.

--- Backend/Rental_Platform_New/prime/views.py
+++ Backend/Rental_Platform_New/prime/views.py
@@ -16,6 +16,7 @@ import json
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from oauth2_provider.models import RefreshToken
 
 User = get_user_model()
 
