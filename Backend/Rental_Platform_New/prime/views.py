ad97993baa5c58b9a568e38f80d6762f313a8fee
valmikGit <quantumcomputingiiitb@gmail.com>
2023-10-02 21:09:09 +0530

    Jyaada kuch add nahi aaya.

--- Backend/Rental_Platform_New/prime/views.py
+++ Backend/Rental_Platform_New/prime/views.py
@@ -18,46 +18,6 @@ from django.http import JsonResponse
 User = get_user_model()
 
 # Create your views here.
-
-# def login_User(request):
-#     if(request.user.is_authenticated):
-#         return redirect('home')
-    
-#     if(request.method == "POST"):
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-    
-#         try:
-#             user = User.objects.get(username=username)
-#         except:
-#             messages.error(request=request, message="Username does not exist")
-        
-#         user = authenticate(request=request, username=username, password=password)
-#         if(user is not None):
-#             login(request=request, user=user)
-#             return redirect('home.html')
-#         else:
-#             messages.error(request=request, message="Username or password is incorrect")
-    
-#     return render(request=request, template_name="login_Page.html")
-
-# def logout_User(request):
-#     logout(request=request)
-#     return redirect('home.html')
-
-# def sign_Up_User(request):
-#     if(request.method == "POST"):
-#         username = request.POST.get('username')
-#         email = request.POST.get('email')
-#         password_1 = request.POST.get('password_1')
-#         password_2 = request.POST.get('password_2')
-#         if(password_1 == password_2):
-#             user = User.objects.create_user(username=username, email=email, password=password_1)
-#             return redirect("login_Page.html")
-#         else:
-#             messages.error(request=request, message="Your password and confirm password do not match.")
-#     return render(request=request, template_name="sign_Up.html")
-
 @api_view(['GET', 'POST'])
 def user_List(request):
     if(request.method == "GET"):
@@ -97,7 +57,9 @@ def delete_Product(request, id):
             messages.success(request=request, message="Deleted successfully")
         else:
             messages.error(request=request, message="Delete unsuccessful")
+
 # Register, Login, User view, logout 
+
 @api_view(['POST'])
 def register_User(request):
     serializer = CustomUser_Serializer(data=request.data)
@@ -171,7 +133,8 @@ def add_To_Cart(request):
         cart_Item.save()
     return JsonResponse(data="It is working", safe=False)
 
-def cart(request):
+@api_view(['GET'])
+def get_Cart(request):
     cart = None
     cartitems = []
 
@@ -184,3 +147,4 @@ def cart(request):
         "items" : cartitems
     }
     return render(request=request, template_name="cart.html")
+
