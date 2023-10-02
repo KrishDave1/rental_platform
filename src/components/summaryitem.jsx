072ab80eaaf66222dc3a5b06653360c3778c63b9
Vaibhav <mittalvaibhav277@gmail.com>
2023-10-02 23:40:02 +0530

    Rent option

--- src/components/summaryitem.jsx
+++ src/components/summaryitem.jsx
@@ -2,14 +2,17 @@ import { useGlobalContext } from "../context";
 import styles from "./summaryitem.module.css";
 
 export default function SummaryItem({ item }) {
-  const { handleDelete, handleAdd, cartItems, Delete,months,setMonths } = useGlobalContext();
-  const PriceRs=(Number(item.Price)*83)/100;
+  const { handleDelete, handleAdd, cartItems, Delete, months, setMonths } =
+    useGlobalContext();
+  const PriceRs = (Number(item.Price) * 83) / 100;
   return (
     <li className={styles.items}>
       <h1 className="text-lg font-bold">{item.Title}</h1>
       <p>Quantity:{cartItems[item.id]}</p>
-      <p>Price:{((item.Price*83)/100 * cartItems[item.id]).toFixed(2)}/mo</p>
-      <p>Total Price for {months} months: ₹ {(PriceRs.toFixed(2)*months*cartItems[item.id]).toFixed(2)}</p>
+      <p>
+        Price:{(((item.Price * 83) / 100) * cartItems[item.id]).toFixed(2)}/mo
+      </p>
+      {/* <p>Total Price for {months} months: ₹ {(PriceRs.toFixed(2)*months*cartItems[item.id]).toFixed(2)}</p> */}
     </li>
   );
 }
